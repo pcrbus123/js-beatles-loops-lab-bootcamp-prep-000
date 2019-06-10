8093705f20dba82e5fb529386155adfb58665d1b
Pnina Breslav <pcrbus123@gmail.com>
2019-06-10 15:21:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // add solution here
-function theBeatlesPlay(){}
+function theBeatlesPlay(muscicians[], instruments[]){
+var newArray= [];
+for(var i = 0; i< musicians.length){
+  newArray.push(musicians[0], musicians [1])
+}
+  
+}
